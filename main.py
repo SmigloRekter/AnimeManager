58628673468457d052fd52552978bf2829640399
Adam Szmigielski <smiglo196@gmail.com>
2018-08-02 21:35:59 +0200

    Divided code to functions, so it is easier to read

--- main.py
+++ main.py
@@ -3,38 +3,43 @@ import sys
 import os,time
 import datetime
 
-
-
+VIDEO_EXTENSION=".mkv"
 MAX_AGE_IN_DAYS=5
-counter=0
 
-if len(sys.argv)!=2:
-    print("Check provided arguments.")
-    sys.exit(0)
+def chechArguments():
+    if len(sys.argv)!=2:
+        print("Check provided arguments.")
+        sys.exit(0)
 
+def openPath(path):
+    try:
+        os.chdir(path)
+    except OSError:
+        print("Could't access selected path. Closing script")
+        sys.exit(0)
+
+def getVideos():
+    listOfFiles=os.listdir(".")
+    filteredListOfFiles=[]
+    for file in listOfFiles:
+        if VIDEO_EXTENSION in file:
+            filteredListOfFiles.append(file)
+    return filteredListOfFiles
+
+def deleteOldVideos(fileList):
+    today = datetime.datetime.now()
+    for file in fileList:
+        fileCreationDate=datetime.datetime.fromtimestamp(os.path.getctime(file))
+        delta=today-fileCreationDate
+        if delta.days>MAX_AGE_IN_DAYS:
+            os.remove(file)
+
+chechArguments()
 path=sys.argv[1]
-try:
-    os.chdir(path)
-except OSError:
-    print("Could't access selected path. Closing script")
-    sys.exit(0)
-
-listOfFiles=os.listdir(".")
-filteredListOfFiles=[]
-for file in listOfFiles:
-    if ".mkv" in file:
-        filteredListOfFiles.append(file)
-
-today = datetime.datetime.now()
-
-for file in filteredListOfFiles:
-    dt=datetime.datetime.fromtimestamp(os.path.getctime(file))
-    delta=today-dt
-    if delta.days>MAX_AGE_IN_DAYS:
-        os.remove(file)
-        counter=counter+1
-    
-print ("Deleted "+str(counter)+" videos")
+openPath(path)
+videos=getVideos()
+deleteOldVideos(videos)   
+print ("Deleted videos")
 
 
 
